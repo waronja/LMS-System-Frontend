d4ea53bd36473198c154e9bdb7009c3c7c13385e
simon waronja <simon.waronja@student.moringaschool.com>
2022-10-24 14:47:01 +0300

    Menubar

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Forgot from './components/Forgot';
+import Layout from './components/Dashboard/Layout';
 
 function App() {
   return (
     <div className="App">
-      
+
+       <Layout/>
        <Login />
        <Signup />
        <Forgot />
+       
     </div>
   );
 }
---
+++ client/src/components/Dashboard/Layout.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+
+function Layout() {
+  return (
+
+<div class = "">
+    
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Dashboard</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Users</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Courses</button>
+  </li>
+  
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">...</div>
+  
+</div>
+
+</div>
+  )
+}
+
+export default Layout
