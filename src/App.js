fd5f661f987d1d287f47bd26c7d25bbf979b797e
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-10-26 22:00:42 +0300

    add instructor code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Route,Routes } from 'react-router-dom';
+//import { Route,Routes } from 'react-router-dom';
 import './App.css';
-import Dashboard from './Pages/Dashboard';
-import Users from './Pages/Users';
-import Login from './components/SignUp-In/Login';
-import Signup from './components/SignUp-In/Signup';
-import Forgot from './components/SignUp-In/Forgot';
-import Layout from './Layout';
-import Metric from './components/Metric';
-import DashSummary from './components/Dashboard/DashSummary';
-import CourseDetails from './components/Course/CourseDetails';
+import Instructor from './components/Instructor/Instructor'
+// import Dashboard from './Pages/Dashboard';
+// import Users from './Pages/Users';
+// import Login from './components/SignUp-In/Login';
+// import Signup from './components/SignUp-In/Signup';
+// import Forgot from './components/SignUp-In/Forgot';
+// import Layout from './Layout';
+// import Metric from './components/Metric';
+// import DashSummary from './components/Dashboard/DashSummary';
+// import CourseDetails from './components/Course/CourseDetails';
 
 function App() {
   return (
     <div className="App">
 
 
-       {/* <CourseDetails/> */}
-       {/* <Layout/>
+       {/* <CourseDetails/> 
+       <Layout/>
        <DashSummary/>
        <Layout/>
        <Metric/>
@@ -27,12 +28,14 @@ function App() {
        <Signup />
        <Forgot /> */}
 
-    <Routes>
+       <Instructor/>
+
+    {/* <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
       </Route>
-    </Routes>
+    </Routes> */}
        
     </div>
   );
---
+++ src/components/Instructor/Instructor.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { Container, Row, Col } from "reactstrap";
+import courseImg1 from "../../assets/web-design.png";
+import courseImg2 from "../../assets/graphics-design.png";
+import courseImg3 from "../../assets/ui-ux.png";
+import InstructorDetails from "./InstructorDetails";
+
+
+const instructorData = [
+  {
+    id: "01",
+    title: "Web Design BootCamp-2022 for Beginners",
+    lesson: 12,
+    students: 12.5,
+    rating: 5.9,
+    imgUrl: courseImg1,
+  },
+
+  {
+    id: "02",
+    title: "Professional Graphics Design, PhotoShop, Adobe XD, Figma",
+    lesson: 12,
+    students: 12.5,
+    rating: 5.9,
+    imgUrl: courseImg2,
+  },
+
+  {
+    id: "03",
+    title: "UI/UX BootCamp for Beginners in 2022",
+    lesson: 12,
+    students: 12.5,
+    rating: 5.9,
+    imgUrl: courseImg3,
+  },
+];
+
+
+function Instructor() {
+  return (
+    <div className='sidebar'>
+      <h1>Instructor's Page</h1>
+      <div className="btns">
+        <button>Add School</button>
+        <button>Add Students</button>
+        <button>Take Attendance</button>
+        <button>Add resources</button>
+        <button>Courses</button>
+        <button>Assignments</button>
+      </div>
+       <section>
+      <Container>
+        <Row>
+          <Col lg="12" className="mb-5">
+            <div className="course__top d-flex justify-content-between align-items-center">
+              <div className="course__top__left w-50">
+                <h2>Our Popular Courses</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+                  consequatur libero quod voluptatibus ullam quia quas, vitae
+                  voluptatem recusandae reprehenderit!
+                </p>
+              </div>
+
+              <div className="w-50 text-end">
+                <button className="btn">See All</button>
+              </div>
+            </div>
+          </Col>
+          {instructorData.map((item) => (
+            <Col lg="4" md="6" sm="6">
+              <InstructorDetails key={item.id} item={item} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
+    </div>
+  )
+}
+
+export default Instructor
