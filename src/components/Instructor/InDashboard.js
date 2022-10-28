b21a27622aa6dd041141f0b190576aafe35f2305
Olga Nadongo <winnienadongo@gmail.com>
2022-10-28 13:32:34 +0300

    adding instructor dashboard and components

---
+++ src/components/Instructor/InDashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './InSideMenu.css'
+import InSideMenu from './InSideMenu'
+
+const InDashboard = () => {
+  return (
+    <div>
+        
+        <InSideMenu/>
+        <h3>Instructor</h3>
+    </div>
+  )
+}
+
+export default InDashboard
