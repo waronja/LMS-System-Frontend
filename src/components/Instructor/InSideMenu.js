ad842f570f7f3f4331bec5de73bb7aed63e3533d
Olga Nadongo <winnienadongo@gmail.com>
2022-10-31 11:53:40 +0300

    adding minimum height to side menu

--- src/components/Instructor/InSideMenu.js
+++ src/components/Instructor/InSideMenu.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function InSideMenu() {
     return (
-    <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{width: '280px', height:'700px'}}>
+    <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{width: '280px', minHeight:'800px'}}>
         <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
             <span className="fs-4">LMS</span>
         </a>
