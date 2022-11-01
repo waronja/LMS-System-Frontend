9a94e0d05bb8169bb30016a7e1bee449bda1545c
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-11-01 14:41:12 +0300

    sign up

--- src/components/Student/StudentForm.js
+++ src/components/Student/StudentForm.js
@@ -1,38 +1,107 @@
 import React from 'react'
+
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function StudentForm() {
+function StudentForm({onLogin}) {
+  const [first_name, setFirstName] =useState("");
+  const [last_name, setLastName] =useState("");
+  const [email, setEmail] =useState(""); 
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+  
+    fetch("https://virtual-backend-app.herokuapp.com/student", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+        
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+        
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+  
+
   return (
     <div>
         
-        <Form>
+        <Form onSubmit={handleSubmit}>
 
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control type="name" placeholder="Student's First Name" />
+                    <Form.Control type="first_name" 
+                     placeholder="Student's First Name"
+                     value ={formData.first_name} 
+                     onChange={(e) => setFirstName(e.target.value)}
+                    //  Id="first_name"
+                     autoComplete="off"
+                    />
                     
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="name" placeholder="Student's Last Name" />
+                    <Form.Control type="last_name" placeholder="Student's Last Name"
+                     value ={last_name} 
+                     onChange={(e) => setLastName(e.target.value)}
+                    //  Id="last_name"
+                     autoComplete="off"
+
+                     />
                     
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Student's Email" />
+                    <Form.Control type="email" placeholder="Student's Email" 
+                    //  id="email"
+                     autoComplete="off"
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password"
+                    //  id="password"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                     autoComplete="current-password"
+
+                     />
                 </Form.Group>
                
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm password" />
+                    <Form.Control type="password" 
+                    placeholder="Confirm password"
+                //    id="password_confirmation"
+                   value={passwordConfirmation}
+                   onChange={(e) => setPasswordConfirmation(e.target.value)}
+                    autoComplete="current-password" 
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -40,8 +109,13 @@ function StudentForm() {
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
-                    Add Student
+                   {isLoading ? "Loading..." : "Add Student"} 
                 </Button>
+                <Form.Group>
+                    {errors.map((err) => (
+                   <error key={err}>{err}</error>
+                     ))}
+                </Form.Group>
             </Form>
 
     </div>
