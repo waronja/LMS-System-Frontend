7d18706d258ba3ef80ec607958747a27c54c2141
Judy Mwai <juddiemukami@gmail.com>
2022-11-02 23:52:58 +0300

    fetch courses

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function UserCard() {
+
+  const [courses, setCourses] = useState([])
+  const getUsers = async () => {
+      const response = await fetch("https://virtual-backend-app.herokuapp.com/courses");
+      const FinalData = await response.json();
+      setCourses(FinalData)
+  }
+  useEffect(() => {
+      getUsers();
+  }, [])
+
+
   return (
     <div>
-     
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>First + Second Name</Card.Title>
-        <Card.Text>
-          Student's Email
-        </Card.Text>
-        <Button variant="primary">Delete</Button>
-      </Card.Body>
-    </Card>
+     {
+        courses.map((course) => {
+          return (
+            <Card style={{ width: '18rem' }} id={course.id} key={course.id}>
+              <Card.Body>
+                <h6>Name: {course.name}</h6>
+                <p>
+                  Description: {course.description}
+                </p>
+                {/* <Button variant="primary">Delete</Button> */}
+              </Card.Body>
+            </Card>
+          // <div className="card_item" id={course.id} key={course.id}>
+          //                 <div className="card_inner">
+          //                     <div className="userName">Name: {course.name}</div>
+          //                     <div className="userName">Description: {course.description}</div>
+          //                     {/* <div className="userName">{course.school.name}</div> */}
+          //                     {/* <div className="userName">{course.school.description}</div> */}
+          //                     {/* <div className="userUrl">{course.student.first_name}</div> */}
+          //                     {/* <div className="userUrl">{course.student.last_name}</div> */}
+          //                     {/* <div className="userUrl">{course.student.email}</div> */}
+                              
+          //                 </div>
+          //     </div>
+          )
+        })
+      }
+    
 
     </div>
   )
